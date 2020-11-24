d5fab450a7ea80ab45246c57533e61f362c0a7f4
AymanMorsy <aymanmorsy.eg@gamil.com>
2020-11-24 19:37:06 +0200

    strapi with quill editor

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e3d92b34b9dc24f4038aaced49157c23'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1393ac29-b5c5-4ad5-af83-3592f76d3244'
+};
